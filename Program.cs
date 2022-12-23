e7e9f3b57873674d64f1ead973e047a84472024f
Taylor N. Albarnaz <veloxgods@gmail.com>
2022-12-23 20:08:17 +0000

    Implementação

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("55923929", "Nokia X6", "358", 64);
+Iphone iphone = new Iphone("5598278337", "Iphone X", "243", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
